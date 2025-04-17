e206ce8ea9438c233a49f876c3bc2fb054fdd685
Xander Fry <afry4321@gmail.com>
2025-04-16 21:08:20 -0500

    Making fixes on the map
    I attempted to fix the Google Map.

--- js/main.js
+++
@@ -1,4 +0,0 @@
-window.onload = function() {
-  const lastModified = document.lastModified;
-  alert("This page was last modified on: " + lastModified);
-};
--- js/map.js
+++
@@ -1,7 +0,0 @@
-function init(){
-  var myMap = new google.maps.Map(document.getElementById('canvas'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8,
-  });
-  
-window.addEventListener('load', init);
