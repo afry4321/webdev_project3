947fe0a303dc76318c462af6788cb987823cd9c5
Xander Fry <afry4321@gmail.com>
2025-04-17 11:37:56 -0500

    Finalized Google Maps API
    Made final fixes to the Google Maps API feature.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-function initMap(){
-  const myMap = new google.maps.Map(document.getElementById("map"), {
-	zoom: 8,
-    center: {lat: -34.397, lng: 150.644},
-  });
-
 window.onload = function() {
   const lastModified = document.lastModified;
   alert("This page was last modified on: " + lastModified);
